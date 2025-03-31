511fc8d6fbe93cb14453a876a9973205b8d40c1b
Denise Aguirre M <denny.aguirre01@gmail.com>
2025-03-31 13:27:24 -0300

    fix api env

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 let apiUrl = "http://localhost:8000/";
 
-if (import.meta.NODE_ENV === "production") {
-  apiUrl = import.meta.VITE_API;
+if (import.meta.env.MODE === "production") {
+  apiUrl = import.meta.env.VITE_API;
 }
 
 export default apiUrl;
